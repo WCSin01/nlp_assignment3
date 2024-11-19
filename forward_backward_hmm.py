382dd27fbf7308d2278f61a815245e952410612a
WC <sinweichuen2001@gmail.com>
2024-11-19 20:15:27 +0000

    save changes

--- forward_backward_hmm.py
+++ forward_backward_hmm.py
@@ -1,4 +1,3 @@
-import pickle
 from dataclasses import dataclass
 from typing import Generic, TypeVar
 
@@ -21,39 +20,47 @@ class HMMParameters:
             pi: np.ndarray,
             transition: np.ndarray,
             emission: np.ndarray):
+        """
+
+        :param pi: initial probability distribution. |POS|
+        :param transition: transition matrix. axis=0: current state, axis=1: next state. |POS|^2
+        :param emission: emission matrix. axis=0: hidden state, axis=1: observed state. |POS| x |vocabulary|
+        """
         assert len(pi.shape) == 1
         POS = pi.shape[0]
         assert transition.shape == (POS, POS)
         assert emission.shape[0] == POS
+
         self.pi = pi
         self.transition = transition
         self.emission = emission
 
 
 class HMM(Generic[TypeT]):
-    def __init__(self, dataset: Dataset[TypeT], pi: np.ndarray, transition: np.ndarray, emission: np.ndarray):
+    def __init__(
+            self,
+            parameters: HMMParameters,
+            dataset: Dataset[TypeT],
+            save_folder_path: str):
         """
 
+        :param parameters:
         :param dataset:
-        :param pi: initial probability distribution. |POS|
-        :param transition: transition matrix. axis=0: current state, axis=1: next state. |POS|^2
-        :param emission: emission matrix. axis=0: hidden state, axis=1: observed state. |POS| x |vocabulary|
+        :param save_folder_path:
         """
-        POS, V = emission.shape
-        assert POS == pi.shape[0]
-        assert (POS, POS) == transition.shape
-        assert np.allclose(pi.sum(), 1)
-        assert np.allclose(transition.sum(axis=1), 1)
-        assert np.allclose(emission.sum(axis=1), 1)
+        assert np.allclose(parameters.pi.sum(), 1)
+        assert np.allclose(parameters.transition.sum(axis=1), 1)
+        assert np.allclose(parameters.emission.sum(axis=1), 1)
 
         self.dataset = dataset
+        self.save_folder_path = save_folder_path
         # np.log(0) = -np.inf
         with np.errstate(divide='ignore', invalid='raise'):
             # increase dimension for mat_mul
-            self.log_pi = np.expand_dims(np.log(pi), axis=0)
-            self.log_transition = np.log(transition)
+            self.log_pi = np.expand_dims(np.log(parameters.pi), axis=0)
+            self.log_transition = np.log(parameters.transition)
             self.log_transition_T = self.log_transition.T
-            self.log_emission_T = np.log(emission).T
+            self.log_emission_T = np.log(parameters.emission).T
 
     def update(self, log_pi, log_transition, log_emission_T):
         self.log_pi = log_pi
@@ -61,13 +68,11 @@ class HMM(Generic[TypeT]):
         self.log_transition_T = self.log_transition.T
         self.log_emission_T = log_emission_T
 
-    def forward_backward(
-            self,
-            max_iter: int) -> HMMParameters:
+    def forward_backward(self, max_iter: int):
         """
 
         :param max_iter: maximum iteration to stop if no convergence.
-        :return: pi, transition, emission, if_converged
+        :return: if_converged
         """
         with np.errstate(divide='ignore', invalid='raise'):
             # one outer loop is one iteration through the whole dataset
@@ -79,16 +84,13 @@ class HMM(Generic[TypeT]):
                 for j, sequence in enumerate(self.dataset.sequences):
                     T = len(sequence)
                     V, _ = self.log_emission_T.shape
-                    encoded_sequence = np.zeros((T, 1, V))
+                    indexed_seq = [self.dataset.ohe.get_index(sequence[t]) for t in range(T)]
 
-                    for t in range(T):
-                        encoded_sequence[t, 0] = self.dataset.ohe.encode_row_log(sequence[t])
-
-                    log_p_j_at_t, log_xi = self.forward_backward_expect(encoded_sequence)
+                    log_p_j_at_t, log_xi = self.forward_backward_expect(indexed_seq)
 
                     new_log_pi, new_log_transition, new_log_emission_T = \
                         self.forward_backward_max(
-                            encoded_sequence,
+                            indexed_seq,
                             new_log_pi,
                             new_log_transition,
                             new_log_emission_T,
@@ -98,7 +100,7 @@ class HMM(Generic[TypeT]):
                     if j % 500 == 0:
                         self.save_sequence_checkpoint(i, max_iter, j, new_log_pi, new_log_transition,
                                                       new_log_emission_T)
-                        
+
                 new_log_pi = log_normalize(new_log_pi, axis=1)
                 new_log_transition = log_normalize(new_log_transition, axis=1)
                 new_log_emission_T = log_normalize(new_log_emission_T, axis=0)
@@ -114,7 +116,7 @@ class HMM(Generic[TypeT]):
 
                 pickle_dump(
                     HMMParameters(new_pi, new_transition, new_emission_T.T),
-                    f"checkpoints/forward_backward/epoch{i}.pkl")
+                    f"{self.save_folder_path}/epoch{i}.pkl")
 
                 if (np.allclose(transition, new_transition) and
                         np.allclose(np.exp(self.log_emission_T), new_emission_T)):
@@ -125,48 +127,47 @@ class HMM(Generic[TypeT]):
 
             return False
 
-    def save_sequence_checkpoint(self, i, max_iter, j, log_pi_numerator, log_transition_numerator,
-                                 log_emission_T_numerator):
-        f = open("checkpoints/forward_backward/checkpoint.txt", "w")
+    def save_sequence_checkpoint(
+            self, i, max_iter, j, log_pi_numerator, log_transition_numerator, log_emission_T_numerator):
+        f = open(f"{self.save_folder_path}/checkpoint.txt", "w")
         f.write(f"i: {i}, j: {j}")
         f.close()
-        np.save(f"checkpoints/forward_backward/log_pi_numerator", log_pi_numerator)
-        np.save(f"checkpoints/forward_backward/log_transition_numerator", log_transition_numerator)
-        np.save(f"checkpoints/forward_backward/log_emission_T_numerator", log_emission_T_numerator)
+        np.save(f"{self.save_folder_path}/log_pi_numerator", log_pi_numerator)
+        np.save(f"{self.save_folder_path}/log_transition_numerator", log_transition_numerator)
+        np.save(f"{self.save_folder_path}/log_emission_T_numerator", log_emission_T_numerator)
         print(f"epoch: {i + 1}/{max_iter}, sequence #: {j + 1}/{len(self.dataset.sequences)}")
 
-    def log_forward(self, encoded_sequence: np.ndarray) -> np.ndarray:
+    def log_forward(self, indexed_seq: list[int]) -> np.ndarray:
         """
 
-        :param encoded_sequence:
+        :param indexed_seq:
         :return: log a_t(j) = log P(o_1 o_2 ... o_t, q_t=j|lambda). Tx1x|POS|
         """
         with np.errstate(divide='ignore', invalid='raise'):
-            T, _, _ = encoded_sequence.shape
+            T = len(indexed_seq)
             _, POS = self.log_pi.shape
 
             # initialize
             # length x POS
             log_p_forward = np.full((T, 1, POS), -np.inf)
 
-            # 1 x V @ V x POS = 1 x POS
             # 1 x POS * 1 x POS
-            log_p_forward[0] = self.log_pi + log_mat_mul(encoded_sequence[0], self.log_emission_T)
+            log_p_forward[0] = self.log_pi + self.log_emission_T[indexed_seq[0]]
 
             # recursion
             for t in range(1, T):
                 # 1 x POS @ POS x POS = 1 x POS
                 log_p_forward[t] = log_mat_mul(log_p_forward[t - 1], self.log_transition) + \
-                                   log_mat_mul(encoded_sequence[t], self.log_emission_T)
+                                   self.log_emission_T[indexed_seq[t]]
             return log_p_forward
 
-    def log_backward(self, encoded_sequence: np.ndarray) -> np.ndarray:
+    def log_backward(self, indexed_seq: list[int]) -> np.ndarray:
         """
 
-        :param encoded_sequence:
+        :param indexed_seq:
         :return: log b_t(i) = log P(o_t+1 o_t+2 ... o_T|q_t = i, lambda). Tx1x|POS|
         """
-        T, _, _ = encoded_sequence.shape
+        T = len(indexed_seq)
         _, POS = self.log_pi.shape
 
         # initialize
@@ -176,13 +177,9 @@ class HMM(Generic[TypeT]):
         # recursion
         # from T-2 to 0 inclusive
         for t in range(T - 2, -1, -1):
-            # 1 x V @ V x POS = 1 x POS
             # 1 x POS @ POS x POS = 1 x POS
             log_p_backward[t] = log_mat_mul(
-                log_mat_mul(
-                    encoded_sequence[t + 1],
-                    self.log_emission_T
-                )+ log_p_backward[t + 1, :],
+                self.log_emission_T[indexed_seq[t + 1]] + log_p_backward[t + 1],
                 self.log_transition_T
             )
         return log_p_backward
@@ -202,7 +199,7 @@ class HMM(Generic[TypeT]):
 
     def expected_state_transition_count(
             self,
-            encoded_sequence: np.ndarray,
+            indexed_seq: list[int],
             log_p_forward: np.ndarray,
             log_p_backward: np.ndarray) -> np.ndarray:
         """
@@ -220,42 +217,34 @@ class HMM(Generic[TypeT]):
 
         for t in range(T - 1):
             # axis 0: from state, axis 1: to state
-            # POS x 1 * POS x POS * (1 x V @ V x POS) * 1 x POS
+            # POS x 1 * POS x POS * 1 x POS * 1 x POS
             log_xi[t] = log_p_forward[t].T + self.log_transition + \
-                        log_mat_mul(encoded_sequence[t + 1], self.log_emission_T) + \
+                        self.log_emission_T[indexed_seq[t + 1]] + \
                         log_p_backward[t + 1]
-
-        # equivalent to:
-        # for t in range(T - 1):
-        #     b_j = log_mat_mul(encoded_sequence[t + 1], self.log_emission_T)[0]
-        #     for i in range(POS):
-        #         for j in range(POS):
-        #             log_xi[t, i, j] = log_p_forward[t, 0, i] + self.log_transition[i, j] + \
-        #                               b_j[j] + log_p_backward[t + 1, 0, j]
 
         # normalize s.t. every time unit sums to 1
         log_xi = log_normalize(log_xi, axis=(1, 2))
         return log_xi
 
-    def forward_backward_expect(self, encoded_sequence: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    def forward_backward_expect(self, indexed_seq: list[int]) -> tuple[np.ndarray, np.ndarray]:
         """
 
-        :param encoded_sequence: T x 1 x |V|
+        :param indexed_seq:
         :return: log_p_j_at_t, log_xi
         """
-        log_p_forward = self.log_forward(encoded_sequence)
-        log_p_backward = self.log_backward(encoded_sequence)
+        log_p_forward = self.log_forward(indexed_seq)
+        log_p_backward = self.log_backward(indexed_seq)
         # p_o_lambda: P(O|lambda)
         # p_o_lambda = np.sum(log_forward_mat[-1])
         # (T-1) x 1 x 1
         log_p_j_at_t = self.expected_state_occupancy_count(log_p_forward, log_p_backward)
         log_xi = self.expected_state_transition_count(
-            encoded_sequence, log_p_forward, log_p_backward)
+            indexed_seq, log_p_forward, log_p_backward)
         return log_p_j_at_t, log_xi
 
     def forward_backward_max(
             self,
-            encoded_sequence: np.ndarray,
+            indexed_seq: list[int],
             log_pi: np.ndarray,
             log_transition: np.ndarray,
             log_emission_T: np.ndarray,
@@ -263,7 +252,7 @@ class HMM(Generic[TypeT]):
             log_xi: np.ndarray):
         """
 
-        :param encoded_sequence: T x 1 x |V|
+        :param encoded_sequence:
         :param log_pi:
         :param log_transition:
         :param log_emission_T:
@@ -271,17 +260,13 @@ class HMM(Generic[TypeT]):
         :param log_xi:
         :return: log_pi, log_transition, log_emission_T
         """
-        encoded_sequence = np.squeeze(encoded_sequence, axis=1)
         log_p_j_at_t = np.squeeze(log_p_j_at_t, axis=1)
 
         log_pi = np.logaddexp(log_pi, log_p_j_at_t[0])
         log_transition = np.logaddexp(log_transition, logsumexp(log_xi, axis=0))
 
-        # V x T @ T x POS = V x POS
-        log_emission_T = np.logaddexp(
-            log_emission_T,
-            log_mat_mul(encoded_sequence.T, log_p_j_at_t)
-        )
+        # V x POS -> T x POS, T x POS
+        log_emission_T[indexed_seq] = np.logaddexp(log_emission_T[indexed_seq], log_p_j_at_t)
         return log_pi, log_transition, log_emission_T
 
 
--- run_forward_backward_hmm.py
+++ run_forward_backward_hmm.py
@@ -1,34 +1,34 @@
 import numpy as np
 from forward_backward_hmm import HMMParameters, seed_matrices, HMM
 from functions import pickle_load
-from process_conllu import ConlluDataset
+from process_conllu import ConlluDataset, OneHot, Dataset
 
 # 35-45 min per iteration
 if __name__ == "__main__":
-    dataset: ConlluDataset = pickle_load("checkpoints/dataset.pkl")
+    # dataset: ConlluDataset = pickle_load("checkpoints/dataset.pkl")
 
     # exclude from training long sentences
-    dataset.sequences = [seq for seq in dataset.sequences if len(seq) < 70]
+    # dataset.sequences = [seq for seq in dataset.sequences if len(seq) < 70]
 
-    parameters: HMMParameters = seed_matrices(len(dataset.upos_set), dataset.vocabulary_size+1)
+    # parameters: HMMParameters = seed_matrices(len(dataset.upos_set), dataset.vocabulary_size+1)
 
-    parameters: HMMParameters = pickle_load("checkpoints/forward_backward_upos/epoch0.pkl")
+    # parameters: HMMParameters = pickle_load("checkpoints/forward_backward_upos/epoch0.pkl")
 
-    pi = parameters.pi
-    transition = parameters.transition
-    emission = parameters.emission
+    pi = np.array([1,0,0])
+    transition = np.array([[0,1,0],
+                           [0,0,1],
+                           [1,0,0]])
+    emission = np.array([[1,0,0],
+                         [0,1,0],
+                         [0,0,1]])
+    dataset = Dataset([[0,1,2]], OneHot([0,1,2]))
 
-    # pi = np.array([1,0,0])
-    # transition = np.array([[0,1,0],
-    #                        [0,0,1],
-    #                        [1,0,0]])
-    # emission = np.array([[1,0,0],
-    #                      [0,1,0],
-    #                      [0,0,1]])
-    # dataset.sequences = np.log(np.array([[[1,0,0],
-    #                                       [0,1,0],
-    #                                       [0,0,1]]]))
-
-    hmm = HMM(dataset, pi, transition, emission)
-    has_converged = hmm.forward_backward(max_iter=7)
+    hmm = HMM(seed_matrices(3,4), dataset, "checkpoints/forward_backward")
+    has_converged = hmm.forward_backward(max_iter=30)
+    print("PI")
+    print(np.exp(hmm.log_pi))
+    print("TRANSITION")
+    print(np.exp(hmm.log_transition))
+    print("EMISSION")
+    print(np.exp(hmm.log_emission_T.T))
     print(f"has convereged: {has_converged}")
